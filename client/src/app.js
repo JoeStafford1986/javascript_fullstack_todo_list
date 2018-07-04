d8ad15bf608434f3fb5995e00fa3789824ebd237
Joe Stafford <joestafford1986@gmail.com>
2018-07-04 14:19:35 +0100

    form view set up

--- client/src/app.js
+++ client/src/app.js
@@ -1,5 +1,6 @@
 const Todo = require('./models/todo.js');
 const TodoListView = require('./views/todo_list_view.js');
+const TodoFormView = require('./views/todo_form_view.js');
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Javascript Loaded');
@@ -7,6 +8,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const todoListContainer = document.querySelector('#todo-list');
   const todoListView = new TodoListView(todoListContainer);
   todoListView.bindEvents();
+
+  const todoListForm = document.querySelector('#todo-form');
+  const todoFormView = new TodoFormView(todoListForm);
+  todoFormView.bindEvents();
 
   const dataURL = 'http://localhost:3000/api/todo';
   const todo = new Todo(dataURL);
---
+++ client/src/views/todo_form_view.js
@@ -0,0 +1,29 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+const TodoFormView = function(form){
+  this.form = form;
+};
+
+TodoFormView.prototype.bindEvents = function () {
+  this.form.addEventListener("submit", (event) =>{
+    this.handleSubmit(event);
+  });
+};
+
+TodoFormView.prototype.handleSubmit = function (event) {
+  event.preventDefault();
+  const newTodo = this.createTodo(event.target);
+  PubSub.publish('TodoFormView:todo-submitted', newTodo);
+  console.log(newTodo);
+  event.target.reset();
+};
+
+TodoFormView.prototype.createTodo = function (form) {
+  const newTodo = {
+    title: form.title.value
+  };
+
+  return newTodo;
+};
+
+module.exports = TodoFormView;
